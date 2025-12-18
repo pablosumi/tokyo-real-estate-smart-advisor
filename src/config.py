0a544d669bef066256a1874b337e342fb810269d
Pablo Sumi <pablo.sumi.1107@gmail.com>
2025-12-18 15:08:25 +0900

    added path constants for train.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@ BASE_URL = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001"
 PREF_CODE = "13"  # Tokyo
 START_YEAR = 2010
 TIMEOUT = 30
+PROCESSED_DATA_PATH = 'data/tokyo-preprocessed.parquet'
+XGB_PARAMS_PATH = 'models/best_hyperparameters_xgb.json'
+MODEL_OUTPUT_PATH = 'models/tokyo_mass_market_xgb.pkl'
